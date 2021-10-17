c34f02a3b717132596d52c897780f59adb3e1190
Maxx Sanner <maxxsanner105@gmail.com>
2021-10-17 18:30:37 -0400

    completed to do list functionality

--- planner/src/components/ToDoList/index.js
+++ planner/src/components/ToDoList/index.js
@@ -4,10 +4,11 @@ import "./style.css";
 function ToDoList() {
     const [toDoItems, setToDoItems] = useState([]);
     const [toDoItem, setToDoItem] = useState('');
-    const [checked, setChecked] = useState("");
+    const [checked, setChecked] = useState(false);
 
     useEffect(() => {
         setInitialLocalStorageArray();
+        // setToDoItemsChecked();
     }, []);
 
     function setInitialLocalStorageArray() {
@@ -19,11 +20,16 @@ function ToDoList() {
     }
 
     function addToDoItem(e) {
-        let toDoItemsArray = toDoItems;
-        toDoItemsArray.push({toDoItem, checked:"true"});
-        localStorage.setItem('react_day_planner_todos', JSON.stringify(toDoItemsArray));
-        setToDoItems(JSON.parse(localStorage.getItem('react_day_planner_todos')));
-        document.getElementById("toDoItem").value = "";
+        if (toDoItem){
+            let toDoItemsArray = toDoItems;
+            if (toDoItemsArray.length != 0) {
+                toDoItemsArray.push({toDoItem, checked, id:toDoItemsArray[toDoItemsArray.length - 1].id + 1});
+            } else toDoItemsArray.push({toDoItem, checked, id:1});
+            localStorage.setItem('react_day_planner_todos', JSON.stringify(toDoItemsArray));
+            setToDoItems(JSON.parse(localStorage.getItem('react_day_planner_todos')));
+            setToDoItem("");
+            document.getElementById("toDoItem").value = "";
+        } else return
     }
 
     function handleToDoItemInputChange(e) {
@@ -31,20 +37,18 @@ function ToDoList() {
     }
 
     function handleToDoItemCheckboxChange(e) {
-        console.log(e.target.checked);
+        let toDoItemsStorage = JSON.parse(localStorage.getItem('react_day_planner_todos'));  
+        let index = toDoItemsStorage.findIndex((obj => obj.id == e.target.value));
+        toDoItemsStorage[index].checked = e.target.checked;
+        localStorage.setItem('react_day_planner_todos', JSON.stringify(toDoItemsStorage));
+        setToDoItems(JSON.parse(localStorage.getItem('react_day_planner_todos')));
     }
     
     function deleteToDoItem(e) {
-        let toDoArray = [];
-        let localToDoItemsStorageArray = JSON.parse(localStorage.getItem('react_day_planner_todos'));
-        for (let i = 0; i < localToDoItemsStorageArray.length; i++) {
-            toDoArray.push(localToDoItemsStorageArray[i].toDoItem)
-        }
-        let index = toDoArray.indexOf(e.target.attributes[1].nodeValue);
-        if (index !== -1) {
-            localToDoItemsStorageArray.splice(index, 1);
-            localStorage.setItem('react_day_planner_todos', JSON.stringify(localToDoItemsStorageArray));
-        }
+        let toDoItemsStorage = JSON.parse(localStorage.getItem('react_day_planner_todos'));  
+        let index = toDoItemsStorage.findIndex((obj => obj.id == e.target.attributes[1].nodeValue));
+        toDoItemsStorage.splice(index, 1);
+        localStorage.setItem('react_day_planner_todos', JSON.stringify(toDoItemsStorage));
         setToDoItems(JSON.parse(localStorage.getItem('react_day_planner_todos')));
     }
 
@@ -79,13 +83,15 @@ function ToDoList() {
                         <tr key={index}>
                             <th className="toDoValue">
                                 <div className="form-check" key={index}>
-                                    <input className="form-check-input" type="checkbox" value="" id={item.toDoItem} onChange={handleToDoItemCheckboxChange}/>
+                                    <input className="form-check-input" type="checkbox" value={item.id} id={item.toDoItem} onChange={handleToDoItemCheckboxChange} 
+                                        checked={item.checked}
+                                    />
                                     <label className="form-check-label">
                                         <strong>{item.toDoItem}</strong>
                                     </label>
                                 </div>
                             </th>
-                            <th className="toDoDelete" ><i className="fas fa-trash-alt" value={item.toDoItem} onClick={deleteToDoItem}></i></th>
+                            <th className="toDoDelete" ><i className="fas fa-trash-alt" value={item.id} id={item.toDoItem} onClick={deleteToDoItem}></i></th>
                         </tr>
                     ))}
                 </tbody>
